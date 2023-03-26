6f5d9c42076c2e7c2489be6314cfe7e284f07395
OksanaZ2608 <oksana26081992@gmail.com>
2023-03-27 00:01:40 +0300

    fix: исправить замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-btn');
@@ -10,24 +9,31 @@ let userName = profile.querySelector('.profile__user');
 let aboutUser = profile.querySelector('.profile__about-user');
 const editButton = profile.querySelector('.profile__edit');
 
-const togglePopupVisibility = function () {
-    popupElement.classList.toggle('popup_is-visible');
+//Открываем Поп-ап и видим значение со страницы
+const openPopup = function () {
+    popupElement.classList.add('popup_is-visible');
+    nameInput.value = userName.textContent;
+    descriptionInput.value = aboutUser.textContent;
 }
 
-editButton.addEventListener('click', togglePopupVisibility);
-popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
+//Закрываем поп-ап без сохранения изменений
+const closePopup = function () {
+    popupElement.classList.remove('popup_is-visible');
+}
 
+editButton.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 
-
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
 function handleFormSubmit(evt) {
-    evt.preventDefault();
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     userName.textContent = nameInput.value;
     aboutUser.textContent = descriptionInput.value;
+    //После отправки формы автоматически закрываем поп-ап
+    closePopup(popupForm);
 }
-
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
 popupForm.addEventListener('submit', handleFormSubmit);
-
-popupSubmitButtonElement.addEventListener('click', togglePopupVisibility);
-
-
 
