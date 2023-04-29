e9615dc4fabbe4c6b7eb3fc3190ff41de4fc13cd
OksanaZ2608 <oksana26081992@gmail.com>
2023-04-29 20:47:57 +0300

    refactor:улучшить код

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,71 +1,69 @@
-const config = {
+function setInputValidState(input, errorElement, parametrs) {
+  input.classList.remove(parametrs.inputErrorClass);
+  errorElement.textContent = '';
+}
+
+function setInputInvalideState(input, errorElement, parametrs) {
+  input.classList.add(parametrs.inputErrorClass);
+  errorElement.textContent = input.validationMessage;
+}
+
+function checkInputValidity(input, form, parametrs) {
+  const errorElement = form.querySelector(`#${input.id}-error`);
+  if (input.checkValidity()) {
+    setInputValidState(input, errorElement, parametrs);
+  } else {
+    setInputInvalideState(input, errorElement, parametrs);
+  }
+}
+
+function disableButton(parametrs, button) {
+  button.setAttribute('disabled', '');
+  button.classList.add(parametrs.inactiveButtonClass);
+}
+
+function enableButton(parametrs, button) {
+  button.removeAttribute('disabled');
+  button.classList.remove(parametrs.inactiveButtonClass);
+}
+
+function toggleButtonValidity(parametrs, form) {
+  const submitButton = form.querySelector(parametrs.submitButtonSelector);
+  if (form.checkValidity()) {
+    enableButton(parametrs, submitButton);
+  } else {
+    disableButton(parametrs, submitButton);
+  }
+}
+
+function setSubmitListener(parametrs, form) {
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    toggleButtonValidity(parametrs, form);
+  });
+}
+
+function enableValidation(parametrs) {
+  const forms = Array.from(document.querySelectorAll(parametrs.formSelector));
+
+  forms.forEach((form) => {
+    setSubmitListener(parametrs, form);
+    const inputs = form.querySelectorAll(parametrs.inputSelector);
+    const inputsArray = Array.from(inputs);
+    inputsArray.forEach(function (input) {
+      input.addEventListener('input', () => {
+        checkInputValidity(input, form, parametrs);
+        toggleButtonValidity(parametrs, form);
+      });
+    });
+  });
+}
+
+enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__submit-btn',
   inactiveButtonClass: 'popup__submit-btn_disable',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__error_visible'
-}
-
-function setInputValidState(input, errorElement) {
-  input.classList.remove(config.inputErrorClass);
-  errorElement.textContent = '';
-}
-
-function setInputInvalideState(input, errorElement) {
-  input.classList.add(config.inputErrorClass);
-  errorElement.textContent = input.validationMessage;
-}
-
-function checkInputValidity(input, form) {
-  const errorElement = form.querySelector(`#${input.id}-error`);
-  if (input.checkValidity()) {
-    setInputValidState(input, errorElement);
-  } else {
-    setInputInvalideState(input, errorElement);
-  }
-}
-
-function disableButton({ inactiveButtonClass }, button) {
-  button.setAttribute('disabled', '');
-  button.classList.add(inactiveButtonClass);
-}
-
-function enableButton({ inactiveButtonClass }, button) {
-  button.removeAttribute('disabled');
-  button.classList.remove(inactiveButtonClass);
-}
-
-function toggleButtonValidity({ submitButtonSelector, ...rest }, form) {
-  const submitButton = form.querySelector(submitButtonSelector);
-  if (form.checkValidity()) {
-    enableButton(rest, submitButton);
-  } else {
-    disableButton(rest, submitButton);
-  }
-}
-
-function setSubmitListener(config, form) {
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    toggleButtonValidity(config, form);
-  });
-}
-
-function enableValidation({ formSelector, inputSelector, ...rest }) {
-  const forms = Array.from(document.querySelectorAll(formSelector));
-
-  forms.forEach((form) => {
-    setSubmitListener(rest, form);
-    const inputs = form.querySelectorAll(inputSelector);
-    const inputsArray = Array.from(inputs);
-    inputsArray.forEach(function (input) {
-      input.addEventListener('input', () => {
-        checkInputValidity(input, form);
-        toggleButtonValidity(rest, form);
-      });
-    });
-  });
-}
-
-enableValidation(config);
+});
